cd5d4f4081072507f35c9d12f91c5f5af2b395fb
Samuel <Semnfumuntoto@hotmail.com>
2025-11-25 15:54:53 +0100

    Restructure PHP files and add homepage
    Renamed PHP files for better organization and consistency. Added index.php as the homepage and created a basic style.css for future styling.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Backend Programming</title>
+    <link rel="stylesheet" href="style. css">
+</head>
+<body>
+    <header>
+        <h1>The Book Backend Programming</h1>
+        <ul>
+            <li><a href="">About</a></li>
+            <li><a href="">Knowledge Test</a></li>
+            <li><a href="">Lab Projects.</a></li>
+        </ul>
+    </header>
+    <main>
+        <section> 
+            <h3>about the page</h3>
+        </section>
+        <section>
+            <h3>1. PHP<h3>
+        </section>
+        <section>
+            <h3>2.DATA ANALYSIS
+        </section>
+        <section> 
+            <h3>3. INTRODUCTION TO MYSQL</h3>
+        </section>
+        <section> 
+            <h3>4. STRUCTURED PROGRAMMING</h3>
+        </section>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
