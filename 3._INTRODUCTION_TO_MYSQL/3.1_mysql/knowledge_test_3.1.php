c904c48f0e0ef3d6d448eab78598f126375fcab2
Samuel <Semnfumuntoto@hotmail.com>
2025-12-10 02:30:26 +0100

    Update MySQL knowledge test questions
    Replaced previous PHP and server-related questions with new questions focused on MySQL concepts, including SQL, auto-increment, database structure, primary keys, data types, and key constraints. Updated the test title to reflect the new content.

--- 3._INTRODUCTION_TO_MYSQL/3.1_mysql/knowledge_test_3.1.php
+++ 3._INTRODUCTION_TO_MYSQL/3.1_mysql/knowledge_test_3.1.php
@@ -7,27 +7,39 @@
     <link rel="stylesheet" href="/backendProgramming/components/knowledge_test.css">
 </head>
 <body>
-    <h1>Knowledge test 1.2</h1>
+    <h1>Knowledge test 3.1</h1>
 
-    <p><b>1. what is a call to undefined function error?</b></p>
+    <p><b>1. What is SQL?</b></p>
+    <p>
+        <b>Answer:</b> SQL stands for Structured Query Language.
+        This is a general-purpose language that allows you to enter and retrieve data from a database.
+    </p>
+
+    <p><b>2. What does auto-increment mean?</b></p>
     <p><b>Answer:</b> </p>
 
-    <p><b>2. what is the phpinfo function?</b></p>
+    <p><b>3. A database consists of...</b></p>
     <p><b>Answer:</b> </p>
 
-    <p><b>3. where can you trace apache server errors?</b></p>
+    <p><b>4. A table consists of...</b></p>
     <p><b>Answer:</b> </p>
 
-    <p><b>4. what is the root directory of the apache server?</b></p>
+    <p><b>5. What is the primary key in the album table ?</b></p>
     <p><b>Answer:</b> </p>
 
-    <p><b>5. list all XAMPP error logs</b></p>
+    <p><b>6. What is the data type of the <em>price</em> attribute in the album table ?</b></p>
     <p><b>Answer:</b> </p>
 
-    <p><b>6. what is a CMS?</b></p>
+    <p><b>7. What is the data type of the primary key ?</b></p>
     <p><b>Answer:</b> </p>
 
-    <p><b>7. what is joomla?</b></p>
+    <p><b>8. what does the referring key do ?</b></p>
+    <p><b>Answer:</b> </p>
+
+    <p><b>9. what are the possible values of SMALLINT ?</b></p>
+    <p><b>Answer:</b> </p>
+
+    <p><b>10. what are the possible values of mediumint ?</b></p>
     <p><b>Answer:</b> </p>
 </body>
 </html>
